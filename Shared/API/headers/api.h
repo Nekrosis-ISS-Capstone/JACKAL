b6a61231712aa8d57689a9ca0abcf19fb1c6b2b4
Scott <pleasantrieshvh@gmail.com>
2024-01-12 00:36:32 -0700

    Fix GetProcessAddress

--- Shared/API/headers/api.h
+++ Shared/API/headers/api.h
@@ -5,15 +5,6 @@
 //#include "../API/ntdll.h"
 namespace API
 {
-	// API_FUNCTIONS defines the function pointer variables of function types defined in *_functions.h
-	typedef struct API_FUNCTIONS
-	{
-		// We need functionality to ensure the addresses of these function pointers are resolved via api hashing
-
-		ptNtQueryInformationProcess_t ptNtQueryInformationProcess;
-
-	}API_FUNCTIONS;
-
 	typedef struct API_MODULES
 	{
 		// We will have a function to get handles to these modules via their hashed value 
@@ -23,34 +14,55 @@ namespace API
 
 	}API_MODULES;
 
-	typedef struct API_MODULE
+	// API_FUNCTIONS defines the function pointer variables of function types defined in *_functions.h
+	typedef struct API_FUNCTIONS
 	{
-		DWORD	 ModuleHash;
-		HMODULE* Module;
-	} API_MODULE;
+		// We need functionality to ensure the addresses of these function pointers are resolved via api hashing
 
-	typedef struct API_T
-	{
-		//DWORD	 FunctionHash;
-		HMODULE* Module;
-		LPVOID*  Function;
-	} API_T;
+		/* NTDLL */
+		ptNtQueryInformationProcess_t ptNtQueryInformationProcess;
+
+	}API_FUNCTIONS;
 
 	typedef struct API_ACCESS
 	{
-		API_FUNCTIONS func;
-		API_MODULES   mod;
+		//API_INIT init;
 
-		API_MODULES LoadModules();
-		void ResolveFunctions(HMODULE hModuleHandle, void* pFunc, const char* szFunc);
+		API_MODULES   mod;
+		API_FUNCTIONS func;
+
 
 	}API_ACCESS;
+	//typedef struct API_MODULE
+	//{
+	//	DWORD	 ModuleHash;
+	//	HMODULE* Module;
+	//} API_MODULE;
 
-	
+	//typedef struct API_T
+	//{
+	//	//DWORD	 FunctionHash;
+	//	HMODULE* Module;
+	//	LPVOID* Function;
+	//} API_T;
 
-	//PVOID GetProcAddress_BinarySearch(PVOID base, const char* func);
-	//void BinarySearch();
-	uintptr_t GetProcessAddress(void *pBase, LPCSTR szFunc);
-	//void  PrintFunctionNames(void* pBaseAddr);
-	uintptr_t HdnGetProcAddress(void* hModule, LPCSTR wAPIName);
+
+	uintptr_t GetProcessAddress(void* pBase, LPCSTR szFunc);
+
+	class APIResolver
+	{
+	public:
+		APIResolver();
+		~APIResolver();
+		
+		API_MODULES FreeModules();
+		const API_ACCESS& GetAPIAccess() const;
+	private:
+
+
+		API_MODULES LoadModules();
+		void ResolveFunctions(API_MODULES hModuleHandle);
+
+		API_ACCESS api;
+	};
 }
--- Shared/utils/src/AntiAnalysis.cpp
+++ Shared/utils/src/AntiAnalysis.cpp
@@ -18,80 +18,36 @@ bool AntiAnalysis::Peb()
 
     // Replace these functions with API hashed functions
     HANDLE  hProcess = GetCurrentProcess();
-    HMODULE hNtdll   = LoadLibraryW(L"ntdll.dll");
 
-    if (!hNtdll)
+    if (api.func.ptNtQueryInformationProcess)
     {
-        tools.ShowError("Failed to get handle to ntdll");
-        return 0;
+        ULONG returnLength;
+        NTSTATUS status = api.func.ptNtQueryInformationProcess(
+            hProcess,
+            ProcessBasicInformation,
+            &pbi,
+            sizeof(pbi),
+            &returnLength
+        );
+
+        if (NT_SUCCESS(status))
+        {
+            if (pbi.PebBaseAddress && pbi.PebBaseAddress->BeingDebugged)
+            {
+                return true;
+            }
+        }
+        else
+        {
+            tools.ShowError("Failed with code: ", GetLastError());
+            return false;
+        }
     }
-    
-    // Get the process address of NtQueryProcessInformation
-   api.func.ptNtQueryInformationProcess = reinterpret_cast<ptNtQueryInformationProcess_t>(
-    GetProcAddress(hNtdll, "NtQueryInformationProcess"));
+    else
+    {
+        tools.ShowError("Failed to get NtQueryInformationProcess address");
+    }
 
-
-   if (api.func.ptNtQueryInformationProcess)
-   {
-       tools.ShowError("GetProc address: ", (int)api.func.ptNtQueryInformationProcess);
-
-   }
-
-
-   // Get the process address of NtQueryProcessInformation
-   api.func.ptNtQueryInformationProcess = reinterpret_cast<ptNtQueryInformationProcess_t>(
-      API::HdnGetProcAddress(hNtdll, "NtQueryInformationProcess"));
-
-
-   if (api.func.ptNtQueryInformationProcess)
-   {
-       tools.ShowError("GetProcessaddress: ", (int)api.func.ptNtQueryInformationProcess);
-
-   }
-
-
-
-
-
-
-
-
-
-
-
-   // if (api.func.ptNtQueryInformationProcess)
-   // {
-   //     ULONG returnLength;
-   //     NTSTATUS status = api.func.ptNtQueryInformationProcess(
-   //         hProcess,
-   //         ProcessBasicInformation,
-   //         &pbi,
-   //         sizeof(pbi),
-   //         &returnLength
-   //     );
-
-   //     if (NT_SUCCESS(status))
-   //     {
-   //         PVOID pPebBeingDebugged = (PPEB)__readgsqword(0x60);
-
-   //         if (pPebBeingDebugged)
-   //         {
-   //             tools.ShowError("Being Debugged");
-   //             return true;
-   //         }
-   //     }
-   //     else
-   //     {
-   //         tools.ShowError("Failed with code: ", GetLastError());
-   //         return false;
-   //     }
-   // }
-   // else
-   // {
-   //     tools.ShowError("Failed to get NtQueryInformationProcess address");
-   // }
-
-   // FreeLibrary(hNtdll);
     return false;
 }
 
